f31ce1778a4f7e6097335f8ac019eb66452f0027
Darragh Blake <darragh.blake48@gmail.com>
2017-10-15 11:22:40 +0100

    Initial Commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -5,6 +5,10 @@
 //  Created by Angela Yu on 27/01/2016.
 //  Copyright © 2016 London App Brewery. All rights reserved.
 //
+//
+//  Skeleton Project cloned on 15/10/2017
+//  Darragh Blake
+//
 
 import UIKit
 
